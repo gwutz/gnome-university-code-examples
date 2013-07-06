a114f9dbb947eec7a63c2198b0b0d0f17881aedd
UserX <userx.gnu@gmail.com>
2013-07-05 21:38:18 -0300

    new file:   notebook/.anjuta/default.profile 	new file:   notebook/.anjuta/session/anjuta.session 	new file:   notebook/.anjuta_sym_db.db 	new file:   notebook/AUTHORS 	new file:   notebook/COPYING 	new file:   notebook/ChangeLog 	new file:   notebook/Debug/Makefile 	new file:   notebook/Debug/config.h 	new file:   notebook/Debug/config.log 	new file:   notebook/Debug/config.status 	new file:   notebook/Debug/libtool 	new file:   notebook/Debug/po/Makefile 	new file:   notebook/Debug/po/Makefile.in 	new file:   notebook/Debug/po/POTFILES 	new file:   notebook/Debug/po/stamp-it 	new file:   notebook/Debug/src/.deps/main.Po 	new file:   notebook/Debug/src/Makefile 	new file:   notebook/Debug/src/main.o 	new file:   notebook/Debug/src/notebook 	new file:   notebook/Debug/stamp-h1 	new file:   notebook/INSTALL 	new file:   notebook/Makefile 	new file:   notebook/Makefile.am 	new file:   notebook/Makefile.in 	new file:   notebook/NEWS 	new file:   notebook/README 	new file:   notebook/aclocal.m4 	new file:   notebook/autogen.sh 	new file:   notebook/autom4te.cache/output.0 	new file:   notebook/autom4te.cache/output.1 	new file:   notebook/autom4te.cache/requests 	new file:   notebook/autom4te.cache/traces.0 	new file:   notebook/autom4te.cache/traces.1 	new file:   notebook/config.guess 	new file:   notebook/config.h 	new file:   notebook/config.h.in 	new file:   notebook/config.log 	new file:   notebook/config.status 	new file:   notebook/config.sub 	new file:   notebook/configure 	new file:   notebook/configure.ac 	new file:   notebook/depcomp 	new file:   notebook/install-sh 	new file:   notebook/intltool-extract.in 	new file:   notebook/intltool-merge.in 	new file:   notebook/intltool-update.in 	new file:   notebook/libtool 	new file:   notebook/ltmain.sh 	new file:   notebook/missing 	new file:   notebook/mkinstalldirs 	new file:   notebook/notebook.anjuta 	new file:   notebook/po/ChangeLog 	new file:   notebook/po/LINGUAS 	new file:   notebook/po/Makefile 	new file:   notebook/po/Makefile.in 	new file:   notebook/po/Makefile.in.in 	new file:   notebook/po/POTFILES 	new file:   notebook/po/POTFILES.in 	new file:   notebook/po/stamp-it 	new file:   notebook/src/.deps/gu_button.Po 	new file:   notebook/src/.deps/gu_note.Po 	new file:   notebook/src/.deps/gu_window.Po 	new file:   notebook/src/.deps/main.Po 	new file:   notebook/src/Makefile 	new file:   notebook/src/Makefile.am 	new file:   notebook/src/Makefile.in 	new file:   notebook/src/gu_button.c 	new file:   notebook/src/gu_button.h 	new file:   notebook/src/gu_button.o 	new file:   notebook/src/gu_note.c 	new file:   notebook/src/gu_note.h 	new file:   notebook/src/gu_note.o 	new file:   notebook/src/gu_window.c 	new file:   notebook/src/gu_window.h 	new file:   notebook/src/gu_window.o 	new file:   notebook/src/main.c 	new file:   notebook/src/main.o 	new file:   notebook/src/notebook 	new file:   notebook/stamp-h1

---
+++ src/notebook/.anjuta/default.profile
@@ -0,0 +1,28 @@
+<?xml version="1.0"?>
+<anjuta>
+    <plugin name="Tools" mandatory="no">
+        <require group="Anjuta Plugin"
+                 attribute="Location"
+                 value="anjuta-tools:ATPPlugin"/>
+    </plugin>
+    <plugin name="Terminal" mandatory="no">
+        <require group="Anjuta Plugin"
+                 attribute="Location"
+                 value="anjuta-terminal:TerminalPlugin"/>
+    </plugin>
+    <plugin name="Git" mandatory="no">
+        <require group="Anjuta Plugin"
+                 attribute="Location"
+                 value="anjuta-git:Git"/>
+    </plugin>
+    <plugin name="GNU Debugger" mandatory="no">
+        <require group="Anjuta Plugin"
+                 attribute="Location"
+                 value="anjuta-gdb:GdbPlugin"/>
+    </plugin>
+    <plugin name="API Help" mandatory="no">
+        <require group="Anjuta Plugin"
+                 attribute="Location"
+                 value="anjuta-devhelp:AnjutaDevhelp"/>
+    </plugin>
+</anjuta>
---
+++ src/notebook/src/main.c
@@ -0,0 +1,49 @@
+/* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*- */
+/*
+ * main.c
+ * Copyright (C) 2013 UserX <userx.gnu@gmail.com>
+ * 
+ * GuNote is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * notebook is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <config.h>
+#include <gtk/gtk.h>
+
+
+#include <glib/gi18n.h>
+
+#include "gu_window.h"
+
+int
+main (int argc, char *argv[])
+{
+ 	GtkWidget *window;
+
+
+#ifdef ENABLE_NLS
+	bindtextdomain (GETTEXT_PACKAGE, PACKAGE_LOCALE_DIR);
+	bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");
+	textdomain (GETTEXT_PACKAGE);
+#endif
+
+	
+	gtk_init (&argc, &argv);
+
+	window = gu_window_new ();
+	gtk_widget_show_all (window);
+
+	gtk_main ();
+
+	return 0;
+}
