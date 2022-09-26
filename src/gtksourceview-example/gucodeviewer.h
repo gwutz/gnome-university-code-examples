c21de9780d0e4995cbe3b95aec371f00bb54b672
Alberto Fanjul <albertofanjul@gmail.com>
2022-09-26 22:53:44 +0200

    whitespace

--- src/gtksourceview-example/gucodeviewer.h
+++ src/gtksourceview-example/gucodeviewer.h
@@ -1,26 +1,25 @@
 /*
  * gucodeviewer.h
- * 
+ *
  * Copyright 2013 userx <userx@AdventChildren>
- * 
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
  * MA 02110-1301, USA.
- * 
- * 
+ *
+ *
  */
-
 
 #ifndef GUCODEVIEWER_H_
 #define GUCODEVIEWER_H_
@@ -30,7 +29,6 @@
 #include <gtksourceview/gtksource.h>
 
 G_BEGIN_DECLS
-
 
 #define GU_TYPE_CODE_VIEWER             (gu_code_viewer_get_type ())
 #define GU_CODE_VIEWER(obj)             (G_TYPE_CHECK_INSTANCE_CAST ((obj), GU_TYPE_CODE_VIEWER, GuCodeViewer))
@@ -46,10 +44,10 @@ typedef struct GuCodeViewerPrivate_  GuCodeViewerPrivate;
 struct GuCodeViewer_
 {
   GtkWindow parent;
-  
-  
+
+
   GuCodeViewerPrivate * priv;
-  
+
 };
 
 struct GuCodeViewerClass_
@@ -62,10 +60,10 @@ GType gu_code_viewer_get_type (void) G_GNUC_CONST;
 GtkWidget *gu_code_viewer_new (void);
 
 // callbacks ::
-void gu_code_viewer_open_file (GtkWidget * widget,
-								gpointer user_data);
+void gu_code_viewer_open_file          (GtkWidget * widget,
+                                        gpointer user_data);
 void gu_code_viewer_set_langhightlight (GtkWidget * widget,
-										gpointer user_data);
+                                        gpointer user_data);
 
 G_END_DECLS
 
